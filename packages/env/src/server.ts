3884906a69addba87a8059820422a44e4064c668
Mukund Mittal <mukund.mukulmittal@gmail.com>
2025-09-14 08:42:04 +0530

    minor changes

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -1,12 +1,5 @@
 import { createEnv } from "@t3-oss/env-core";
-import dotenv from "dotenv";
 import { z } from "zod";
-
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({
-    path: "../../.env",
-  });
-}
 
 export const serverEnv = createEnv({
   server: {
